3a9dbb350a9d256cf462266953267399655125f6
Alejandro Seguí <alesegdia@gmail.com>
2016-10-29 15:12:23 +0200

    adding points to unlock to bikes

--- src/core/bikeparams.h
+++ src/core/bikeparams.h
@@ -6,6 +6,7 @@ struct BikeParams
 	float m_accel;
 	float m_maxSpeed;
 	float m_slide;
+	int m_ptsToUnlock;
 };
 
 class BikeParamsHolder
@@ -13,9 +14,9 @@ class BikeParamsHolder
 public:
 	static void Initialize()
 	{
-		s_bikes[0] = { 1, 2, 3 };
-		s_bikes[1] = { 2, 1, 4 };
-		s_bikes[2] = { 4, 2, 1 };
+		s_bikes[0] = { 1, 2, 3, 0 };
+		s_bikes[1] = { 2, 1, 4, 300 };
+		s_bikes[2] = { 4, 2, 1, 600 };
 	}
 
 	static BikeParams s_bikes[3];
