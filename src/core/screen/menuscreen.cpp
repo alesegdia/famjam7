a71ebb54effa5787c374dad003200fc1121eef5c
Alejandro Seguí <alesegdia@gmail.com>
2016-10-29 03:49:54 +0200

    adding menuscreen

---
+++ src/core/screen/menuscreen.cpp
@@ -0,0 +1,6 @@
+#include "menuscreen.h"
+
+MenuScreen::MenuScreen()
+{
+
+}
---
+++ src/core/screen/menuscreen.h
@@ -0,0 +1,11 @@
+#ifndef MENUSCREEN_H
+#define MENUSCREEN_H
+
+
+class MenuScreen
+{
+public:
+	MenuScreen();
+};
+
+#endif // MENUSCREEN_H
